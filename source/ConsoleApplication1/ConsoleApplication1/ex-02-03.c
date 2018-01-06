77aef31e977061fd47f3f0b058147e36b5e4a498
chang1111 <dream134@naver.com>
2018-01-06 14:12:41 +0900

    backup 14:12

---
+++ source/ConsoleApplication1/ConsoleApplication1/ex-02-03.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("sangbeom.jung\n010-3942-1039\n");
+	system("pause");
+}
---
+++ work/ConsoleApplication3/ConsoleApplication1/ex02-01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int i = 0;
+	printf("hello, world!!");
+	
+	i = 10;
+	
+	printf("%d", i);
+
+	system("pause");
+	return;
+}
